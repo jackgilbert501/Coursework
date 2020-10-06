b5cfeecfd7a9bd7fd223a916be866c8e1d6413e4
92290 <92290@farnborough.ac.uk>
2020-10-06 12:06:26 +0100

    Added Users API methods

---
+++ src/main/java/controllers/Users.java
@@ -0,0 +1,103 @@
+package controllers;
+
+import org.glassfish.jersey.media.multipart.FormDataParam;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import server.Main;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+@Path("users/")
+@Consumes(MediaType.MULTIPART_FORM_DATA)
+@Produces(MediaType.APPLICATION_JSON)
+
+public class Users{
+    @GET
+    @Path("list")
+    public String UsersList() {
+        System.out.println("Invoked Users.UsersList()");
+        JSONArray response = new JSONArray();
+        try {
+            PreparedStatement ps = Main.db.prepareStatement("SELECT UserID, UserName, Password, Coach FROM Users");
+            ResultSet results = ps.executeQuery();      // I am using CamelCase for the column names to make it consistant
+            while (results.next()==true) {
+                JSONObject row = new JSONObject();
+                row.put("UserID", results.getInt(1));
+                row.put("UserName", results.getString(2));
+                row.put("Password", results.getString(3));
+                row.put("Coach", results.getBoolean(4));
+                response.add(row);
+            }
+            return response.toString();
+        } catch (Exception exception) {
+            System.out.println("Database error: " + exception.getMessage());
+            return "{\"Error\": \"Unable to list items.  Error code xx.\"}";
+        }
+    }
+
+
+    @POST
+    @Path("add")
+    public String UsersAdd(@FormDataParam("UserName") String UserName, @FormDataParam("Password") String Password, @FormDataParam("Coach") Boolean Coach) {
+        System.out.println("Invoked Users.UsersAdd()");
+        try {
+            PreparedStatement ps = Main.db.prepareStatement("INSERT INTO Users (UserName, Password, Coach) VALUES (?, ?, ?)");
+            ps.setString(1, UserName);
+            ps.setString(2, Password);
+            ps.setBoolean(3, Coach);
+            ps.execute();
+            return "{\"OK\": \"Added user.\"}";
+        } catch (Exception exception) {
+            System.out.println("Database error: " + exception.getMessage());
+            return "{\"Error\": \"Unable to create new item, please see server console for more info.\"}";
+        }
+    }
+    @POST
+    @Path("update")
+    public String updateFood(@FormDataParam("UserID") Integer UserID, @FormDataParam("UserName") String UserName, @FormDataParam("Password") String Password, @FormDataParam("Coach") Boolean Coach) {
+        try {
+            System.out.println("Invoked Users.UpdateUsers/update UserID=" + UserID);
+            PreparedStatement ps = Main.db.prepareStatement("UPDATE Users SET UserName = ? WHERE UserID = ?");
+            ps.setString(1, UserName);
+            ps.setInt(2, UserID);
+            ps.execute();
+            return "{\"OK\": \"Users updated\"}";
+        } catch (Exception exception) {
+            System.out.println("Database error: " + exception.getMessage());
+            return "{\"Error\": \"Unable to update item, please see server console for more info.\"}";
+        }
+    }
+
+
+    @POST
+    @Path("delete/{UserID}")
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    @Produces(MediaType.APPLICATION_JSON)
+    public String DeleteUser(@PathParam("UserID") Integer UserID) throws Exception {
+        System.out.println("Invoked Users.DeleteUser()");
+        if (UserID == null) {
+            throw new Exception("UserID is missing in the HTTP request's URL.");
+        }
+        try {
+            PreparedStatement ps = Main.db.prepareStatement("DELETE FROM Users WHERE UserID = ?");
+            ps.setInt(1, UserID);
+            ps.execute();
+            return "{\"OK\": \"User deleted\"}";
+        } catch (Exception exception) {
+            System.out.println("Database error: " + exception.getMessage());
+            return "{\"Error\": \"Unable to delete item, please see server console for more info.\"}";
+        }
+    }
+
+
+
+
+}
+
+
+
+
+// push
