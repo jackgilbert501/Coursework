c2989c255d63bcb8e030f7879476083836997676
jackgilbert501 <92290@farnborough.ac.uk>
2020-11-20 14:35:42 +0000

    added users login

--- src/main/java/controllers/Drills.java
+++ src/main/java/controllers/Drills.java
@@ -1,4 +1,16 @@
 package controllers;
 
+import server.Main;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 public class Drills {
+
+    PreparedStatement statement = Main.db.prepareStatement(   //using public connection
+            "SELECT DrillID,  FROM  WHERE  LIKE ?"
+    );
+statement.setString(1, '%' + searchValue.toLowerCase() + '%');  //% is wildcard so FoodName contains search value
+    ResultSet resultSet = statement.executeQuery();
+
 }
--- src/main/java/controllers/Users.java
+++ src/main/java/controllers/Users.java
@@ -9,6 +9,7 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.UUID;
 
 @Path("users/")
 @Consumes(MediaType.MULTIPART_FORM_DATA)
@@ -121,6 +122,38 @@ public class Users{
 
 
 
+
+    @POST
+    @Path("login")
+    public String UsersLogin(@FormDataParam("UserName") String UserName, @FormDataParam("PassWord") String PassWord) {
+        System.out.println("Invoked loginUser() on path users/login");
+        try {
+            PreparedStatement ps1 = Main.db.prepareStatement("SELECT PassWord FROM Users WHERE UserName = ?");
+            ps1.setString(1, UserName);
+            ResultSet loginResults = ps1.executeQuery();
+            if (loginResults.next() == true) {
+                String correctPassword = loginResults.getString(1);
+                if (PassWord.equals(correctPassword)) {
+                    String Token = UUID.randomUUID().toString();
+                    PreparedStatement ps2 = Main.db.prepareStatement("UPDATE Users SET Token = ? WHERE UserName = ?");
+                    ps2.setString(1, Token);
+                    ps2.setString(2, UserName);
+                    ps2.executeUpdate();
+                    JSONObject userDetails = new JSONObject();
+                    userDetails.put("UserName", UserName);
+                    userDetails.put("Token", Token);
+                    return userDetails.toString();
+                } else {
+                    return "{\"Error\": \"Incorrect password!\"}";
+                }
+            } else {
+                return "{\"Error\": \"Incorrect username.\"}";
+            }
+        } catch (Exception exception) {
+            System.out.println("Database error during /users/login: " + exception.getMessage());
+            return "{\"Error\": \"Server side error!\"}";
+        }
+    }
 
 }
 
