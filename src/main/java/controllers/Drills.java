597e16c0f4d7fd7102c8abbcac9e36911c5e0e1b
92290 <92290@farnborough.ac.uk>
2020-12-04 16:09:29 +0000

    Added drills list

--- src/main/java/controllers/Drills.java
+++ src/main/java/controllers/Drills.java
@@ -1,16 +1,43 @@
 package controllers;
 
+import org.glassfish.jersey.media.multipart.FormDataParam;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import server.Main;
 
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.UUID;
 
+@Path("drills/")
+@Consumes(MediaType.MULTIPART_FORM_DATA)
+@Produces(MediaType.APPLICATION_JSON)
 public class Drills {
 
-    PreparedStatement statement = Main.db.prepareStatement(   //using public connection
-            "SELECT DrillID,  FROM  WHERE  LIKE ?"
-    );
-//statement.setString(1, '%' + searchValue.toLowerCase() + '%');  //% is wildcard so FoodName contains search value
-    ResultSet resultSet = statement.executeQuery();
 
+
+    @GET
+    @Path("list")
+    public String UsersList() {
+        System.out.println("Invoked Users.UsersList()");
+        JSONArray response = new JSONArray();
+        try {
+            PreparedStatement ps = Main.db.prepareStatement("SELECT DrilllID, DrillName, Description, Rating FROM Users");
+            ResultSet results = ps.executeQuery();      // I am using CamelCase for the column names to make it consistant
+            while (results.next()==true) {
+                JSONObject row = new JSONObject();
+                row.put("DrilllID", results.getInt(1));
+                row.put("DrillName", results.getString(2));
+                row.put("Description", results.getString(3));
+                row.put("Rating", results.getInt(4));
+                response.add(row);
+            }
+            return response.toString();
+        } catch (Exception exception) {
+            System.out.println("Database error: " + exception.getMessage());
+            return "{\"Error\": \"Unable to list items.  Error code xx.\"}";
+        }
+    }
 }
--- src/main/java/controllers/Users.java
+++ src/main/java/controllers/Users.java
@@ -125,15 +125,15 @@ public class Users{
 
     @POST
     @Path("login")
-    public String UsersLogin(@FormDataParam("UserName") String UserName, @FormDataParam("PassWord") String PassWord) {
+    public String UsersLogin(@FormDataParam("UserName") String UserName, @FormDataParam("Password") String Password) {
         System.out.println("Invoked loginUser() on path users/login");
         try {
-            PreparedStatement ps1 = Main.db.prepareStatement("SELECT PassWord FROM Users WHERE UserName = ?");
+            PreparedStatement ps1 = Main.db.prepareStatement("SELECT Password FROM Users WHERE UserName = ?");
             ps1.setString(1, UserName);
             ResultSet loginResults = ps1.executeQuery();
             if (loginResults.next() == true) {
                 String correctPassword = loginResults.getString(1);
-                if (PassWord.equals(correctPassword)) {
+                if (Password.equals(correctPassword)) {
                     String Token = UUID.randomUUID().toString();
                     PreparedStatement ps2 = Main.db.prepareStatement("UPDATE Users SET Token = ? WHERE UserName = ?");
                     ps2.setString(1, Token);
