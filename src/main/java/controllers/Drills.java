9a0de7a8dc2ac73b6fa61ab28a1ec0b034a51c01
jackgilbert501 <92290@farnborough.ac.uk>
2020-12-15 12:32:52 +0000

    added drills list, delte add

--- src/main/java/controllers/Drills.java
+++ src/main/java/controllers/Drills.java
@@ -12,23 +12,27 @@ import java.sql.ResultSet;
 import java.util.UUID;
 
 @Path("drills/")
-@Consumes(MediaType.MULTIPART_FORM_DATA)
-@Produces(MediaType.APPLICATION_JSON)
-public class Drills {
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    @Produces(MediaType.APPLICATION_JSON)
+    public class Drills {
+    private Integer drillID;
+    private String drillName;
+    private String description;
+    private Integer rating;
 
 //test
 
     @GET
     @Path("list")
-    public String UsersList() {
-        System.out.println("Invoked Users.UsersList()");
+    public String DrillList() {
+        System.out.println("Invoked Drills.DrillsList()");
         JSONArray response = new JSONArray();
         try {
-            PreparedStatement ps = Main.db.prepareStatement("SELECT DrilllID, DrillName, Description, Rating FROM Users");
+            PreparedStatement ps = Main.db.prepareStatement("SELECT DrillID, DrillName, Description, Rating FROM Drills");
             ResultSet results = ps.executeQuery();      // I am using CamelCase for the column names to make it consistant
             while (results.next()==true) {
                 JSONObject row = new JSONObject();
-                row.put("DrilllID", results.getInt(1));
+                row.put("DrillID", results.getInt(1));
                 row.put("DrillName", results.getString(2));
                 row.put("Description", results.getString(3));
                 row.put("Rating", results.getInt(4));
@@ -40,4 +44,58 @@ public class Drills {
             return "{\"Error\": \"Unable to list items.  Error code xx.\"}";
         }
     }
+
+
+    @POST
+    @Path("add")
+    public String DrillsAdd(@FormDataParam("DrillID") Integer DrillID, @FormDataParam("DrillName") String DrillName, @FormDataParam("Description") String Description, @FormDataParam("Rating") Integer Rating) {
+        drillID = DrillID;
+        drillName = DrillName;
+        description = Description;
+        rating = Rating;
+        System.out.println("Invoked Drills.DrillsAdd()");
+        try {
+            PreparedStatement ps = Main.db.prepareStatement("INSERT INTO Drills (DrillName, Description, Rating) VALUES (?, ?, ?)");
+
+            ps.setString(1, DrillName);
+            ps.setString(2, Description);
+            ps.setInt(3, Rating);
+
+            ps.execute();
+            return "{\"OK\": \"Added drill.\"}";
+        } catch (Exception exception) {
+            System.out.println("Database error: " + exception.getMessage());
+            return "{\"Error\": \"Unable to create new item, please see server console for more info.\"}";
+        }
+
+    }
+
+
+    @POST
+    @Path("delete/{DrillID}")
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    @Produces(MediaType.APPLICATION_JSON)
+    public String DeleteDrill(@PathParam("DrillID") Integer DrillID) throws Exception {
+        System.out.println("Invoked Drills.DeleteDrill()");
+        if (DrillID == null) {
+            throw new Exception("DrillID is missing in the HTTP request's URL.");
+        }
+        try {
+            PreparedStatement ps = Main.db.prepareStatement("DELETE FROM Drills WHERE DrillID = ?");
+            ps.setInt(1, DrillID);
+            ps.execute();
+            return "{\"OK\": \"Drill deleted\"}";
+        } catch (Exception exception) {
+            System.out.println("Database error: " + exception.getMessage());
+            return "{\"Error\": \"Unable to delete item, please see server console for more info.\"}";
+        }
+    }
+
+
+
+
+
+
+
+
 }
