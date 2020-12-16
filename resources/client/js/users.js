34b31cd784acf6845bfeb8010bf2ec73c577e1de
jackgilbert501 <92290@farnborough.ac.uk>
2020-12-16 16:12:13 +0000

    added drills list, delte add

--- resources/client/js/users.js
+++ resources/client/js/users.js
@@ -14,7 +14,36 @@ function getUsersList() {
             formatUsersList(response);          //this function will create an HTML table of the data (as per previous lesson)
         }
     });
-}   
+}
+
+function UsersLogin() {
+    //debugger;
+    console.log("Invoked UsersLogin() ");
+    let url = "/users/login";
+    let formData = new FormData(document.getElementById('LoginForm'));
+
+    fetch(url, {
+        method: "POST",
+        body: formData,
+    }).then(response => {
+        return response.json();                 //now return that promise to JSON
+    }).then(response => {
+        if (response.hasOwnProperty("Error")) {
+            alert(JSON.stringify(response));        // if it does, convert JSON object to string and alert
+        } else {
+            Cookies.set("Token", response.Token);
+            Cookies.set("UserName", response.UserName);
+            window.open("index.html", "_self");       //open index.html in same tab
+        }
+    });
+}
+
+
+
+
+
+
+
 
 
 
